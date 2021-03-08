bac018d85430f819fe6264513f58a631feb43b7f
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-03-07 21:37:54 -0500

    Add guacamole deployment

---
+++ k8s-235/apps/guacamole/terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    endpoint = "https://spitfire.235.tdude.co:9000/"
+    bucket   = "k8s-235-guacamole"
+    key      = "terraform.tfstate"
+    region   = "main"
+    # We are using minio
+    force_path_style = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+  }
+}
---
+++ k8s-235/apps/guacamole/terraform/dns.tf
@@ -0,0 +1,14 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "guacamole-app" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "guacamole"
+  value   = "traefik.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
