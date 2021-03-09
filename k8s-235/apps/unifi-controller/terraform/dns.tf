20a7ab3dc6136319f73b7dca2736ee6451f2f1f7
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-03-09 04:33:17 -0500

    Added Unifi controller deployment

---
+++ k8s-235/apps/unifi-controller/terraform/dns.tf
@@ -0,0 +1,41 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "unifi" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "unifi"
+  value   = "traefik.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "unifi-235" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "unifi.235"
+  value   = "unifi.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "unifi-260" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "unifi.260"
+  value   = "unifi.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "unifi-305-1700" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "unifi.305-1700"
+  value   = "unifi.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
