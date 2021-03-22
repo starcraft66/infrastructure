f41f8bdcdeb022d47f27aed625c0aabaf8043589
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-03-22 00:15:58 -0400

    Add some minecraft servers

---
+++ k8s-235/apps/minecraft/ftb-ultimate/terraform/dns.tf
@@ -0,0 +1,40 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "ftb-ultimate-srv" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "_minecraft._tcp.ftb-ultimate"
+  data    = {
+    name      = "ftb-ultimate"
+    port      = 12346
+    priority  = 1
+    proto     = "_tcp"
+    service   = "_minecraft"
+    target    = "ftb-ultimate.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+    weight    = 1
+  }
+  type    = "SRV"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "ftb-ultimate-v4" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "ftb-ultimate.k8s.235"
+  value   = "24.225.136.165"
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "ftb-ultimate-v6" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "ftb-ultimate.k8s.235"
+  value   = "2607:fa48:6ed8:8a54:3::3"
+  type    = "AAAA"
+  ttl     = 1
+  proxied = false
+}
---
+++ k8s-235/apps/minecraft/skyfactory4/terraform/dns.tf
@@ -0,0 +1,40 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "skyfactory4-srv" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "_minecraft._tcp.skyfactory4"
+  data    = {
+    name      = "skyfactory4"
+    port      = 12347
+    priority  = 1
+    proto     = "_tcp"
+    service   = "_minecraft"
+    target    = "skyfactory4.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+    weight    = 1
+  }
+  type    = "SRV"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "skyfactory4-v4" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "skyfactory4.k8s.235"
+  value   = "24.225.136.165"
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "skyfactory4-v6" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "skyfactory4.k8s.235"
+  value   = "2607:fa48:6ed8:8a54:3::4"
+  type    = "AAAA"
+  ttl     = 1
+  proxied = false
+}
