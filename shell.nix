68e773d233526bcbf5d2c520058042a38200dca1
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-07-21 22:14:37 -0400

    nix-shell: update nixpkgs + kustomize 4

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 { isDev ? true
-, pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/176690a7630f4ec253bba538fe24086750decc6e.tar.gz") { }
+, pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/16105403bdd843540cbef9c63fc0f16c1c6eaa70.tar.gz") { }
 }:
 
 with pkgs;
 mkShell rec {
   buildInputs = [
     ansible
-    kubectl                    # v1.18.1
-    kubernetes-helm            # v3.2.1
-    terraform_0_14             # v0.12.28
+    kubectl
+    kubernetes-helm
+    terraform_1_0
     sops
     kustomize
     kubetail
@@ -16,8 +16,8 @@ mkShell rec {
     yamllint
     gnupg
   ] ++ lib.optional isDev [
-    kubectx                    # v0.9.0
-    kind                       # v0.8.1
+    kubectx
+    kind
   ];
 
   KUSTOMIZE_PLUGIN_HOME = pkgs.buildEnv {
