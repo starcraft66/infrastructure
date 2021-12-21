e96b5bf2c239a604b36b2e7dc96c1c920dfe9b18
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-12-20 20:36:20 -0500

    beyondroastbeef: initial deployment

---
+++ k8s-235/apps/minecraft/beyondroastbeef/terraform/dns.tf
@@ -0,0 +1,40 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "beyondroastbeef-srv" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "_minecraft._tcp.beyondroastbeef"
+  data {
+    name     = "beyondroastbeef"
+    port     = 12348
+    priority = 1
+    proto    = "_tcp"
+    service  = "_minecraft"
+    target   = "beyondroastbeef.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+    weight   = 1
+  }
+  type    = "SRV"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "beyondroastbeef-v4" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "beyondroastbeef.k8s.235"
+  value   = "24.225.136.165"
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+# resource "cloudflare_record" "beyondroastbeef-v6" {
+#   zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+#   name    = "beyondroastbeef.k8s.235"
+#   value   = "2607:fa48:6ed8:8a54:3::2"
+#   type    = "AAAA"
+#   ttl     = 1
+#   proxied = false
+# }
