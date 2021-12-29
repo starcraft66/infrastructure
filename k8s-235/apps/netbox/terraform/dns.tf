a7f992358371eccbc3863fe5be2f4210ac4104b9
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-12-29 01:58:14 -0500

    netbox: fix typo in terraform

--- k8s-235/apps/netbox/terraform/dns.tf
+++ k8s-235/apps/netbox/terraform/dns.tf
@@ -4,7 +4,7 @@ data "cloudflare_zones" "tdude_co" {
   }
 }
 
-resource "cloudflare_record" "nextcloud" {
+resource "cloudflare_record" "netbox" {
   zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
   name    = "netbox"
   value   = "traefik.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
