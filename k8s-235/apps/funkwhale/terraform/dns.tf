ef69d591bff025a3a522f7971738640c3a1aec99
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-12-29 01:57:18 -0500

    funkwhale: fix typo in terraform

--- k8s-235/apps/funkwhale/terraform/dns.tf
+++ k8s-235/apps/funkwhale/terraform/dns.tf
@@ -4,7 +4,7 @@ data "cloudflare_zones" "tdude_co" {
   }
 }
 
-resource "cloudflare_record" "nextcloud" {
+resource "cloudflare_record" "funkwhale" {
   zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
   name    = "music"
   value   = "traefik.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
