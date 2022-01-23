e54d807a4300904a4850c6c67f4863b3225ee5ca
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-22 20:33:47 -0500

    cacaffichage: expose dynmap web interface

--- k8s-235/apps/minecraft/cacaffichage/terraform/dns.tf
+++ k8s-235/apps/minecraft/cacaffichage/terraform/dns.tf
@@ -23,6 +23,24 @@ resource "cloudflare_record" "cacaffichage-srv" {
 
 resource "cloudflare_record" "cacaffichage-v4" {
   zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "cacaffichage"
+  value   = "24.225.136.165"
+  type    = "A"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "cacaffichage-v6" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "cacaffichage"
+  value   = "2607:fa48:6ed8:8a54:3::1"
+  type    = "AAAA"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "cacaffichage-v4" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
   name    = "cacaffichage.k8s.235"
   value   = "24.225.136.165"
   type    = "A"
