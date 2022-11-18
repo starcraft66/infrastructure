1f6a905a88cb40b1851163eceb923ad24b905e76
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-11-17 20:54:53 -0500

    dns: decom lhr-r1

--- dns/dns_as208914.net.tf
+++ dns/dns_as208914.net.tf
@@ -16,28 +16,10 @@ resource "cloudflare_record" "lg_as208914_net-AAAA" {
   proxied = false
 }
 
-resource "cloudflare_record" "lhr-r1_as208914_net-AAAA" {
-  zone_id = cloudflare_zone.as208914_net.id
-  name    = "lhr-r1"
-  value   = "2a10:4741:21::1"
-  type    = "AAAA"
-  ttl     = 1
-  proxied = false
-}
-
 resource "cloudflare_record" "yyz-r1_as208914_net-AAAA" {
   zone_id = cloudflare_zone.as208914_net.id
   name    = "yyz-r1"
   value   = "2a10:4741:33::1"
-  type    = "AAAA"
-  ttl     = 1
-  proxied = false
-}
-
-resource "cloudflare_record" "lhr-r1_backdoor_as208914_net-AAAA" {
-  zone_id = cloudflare_zone.as208914_net.id
-  name    = "lhr-r1.backdoor"
-  value   = "2a10:4740:40:0:2222:5dd3:df75:1"
   type    = "AAAA"
   ttl     = 1
   proxied = false
@@ -51,16 +33,6 @@ resource "cloudflare_record" "yyz-r1_backdoor_as208914_net-AAAA" {
   ttl     = 1
   proxied = false
 }
-
-resource "cloudflare_record" "lhr-r1_backdoor_as208914_net-A" {
-  zone_id = cloudflare_zone.as208914_net.id
-  name    = "lhr-r1.backdoor"
-  value   = "103.175.234.157"
-  type    = "A"
-  ttl     = 1
-  proxied = false
-}
-
 resource "cloudflare_record" "yyz-r1_backdoor_as208914_net-A" {
   zone_id = cloudflare_zone.as208914_net.id
   name    = "yyz-r1.backdoor"
