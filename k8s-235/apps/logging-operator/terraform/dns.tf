f1445db4bd06d2a018776e3ea6ce44afeea20177
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-01-04 03:07:02 -0500

    logging-operator: initial deployment

---
+++ k8s-235/apps/logging-operator/terraform/dns.tf
@@ -0,0 +1,14 @@
+data "cloudflare_zones" "tdude_co" {
+  filter {
+    name = "tdude.co"
+  }
+}
+
+resource "cloudflare_record" "kibana" {
+  zone_id = lookup(data.cloudflare_zones.tdude_co.zones[0], "id")
+  name    = "logs.monitoring"
+  value   = "traefik.k8s.235.${lookup(data.cloudflare_zones.tdude_co.zones[0], "name")}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = false
+}
