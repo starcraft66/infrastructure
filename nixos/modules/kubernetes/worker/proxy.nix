cb0b45c99dbdf3cee396de7b2d0033ef5d00df13
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-12-01 02:48:02 -0500

    kubernetes: fix kube-proxy hostname config

--- nixos/modules/kubernetes/worker/proxy.nix
+++ nixos/modules/kubernetes/worker/proxy.nix
@@ -6,6 +6,7 @@ in {
   services.kubernetes.proxy = lib.mkIf cfg.kube-proxy.enable {
     enable = true;
     bindAddress = "::";
+    hostname = "${config.networking.hostName}.235.tdude.co";
     kubeconfig = {
       caFile = "/var/lib/secrets/kubernetes/kubernetes-ca.pem";
       certFile = "/var/lib/secrets/kubernetes/kube-proxy.pem";
