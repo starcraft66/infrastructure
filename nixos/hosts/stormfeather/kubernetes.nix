8c33b028b40c1496da097b1f3e7639705ba33aff
Tristan Gosselin-Hane <starcraft66@gmail.com>
2023-12-02 03:15:12 -0500

    nixos: fix stormfeather k8s config

--- nixos/hosts/stormfeather/kubernetes.nix
+++ nixos/hosts/stormfeather/kubernetes.nix
@@ -11,11 +11,9 @@
   services.tdude.kubernetes.control-plane.serviceCidrIpv4 = "10.234.64.0/18";
   services.tdude.kubernetes.control-plane.serviceCidrIpv6 = "2a10:4741:36:32:1::/112";
   services.tdude.kubernetes.worker.ipSans = [
-    # (builtins.elemAt config.networking.interfaces."eno1.29".ipv4.addresses 0).address
-    # (builtins.elemAt config.networking.interfaces."eno1.29".ipv6.addresses 0).address
-    (builtins.elemAt config.networking.interfaces."ens18".ipv4.addresses 0).address
-    (builtins.elemAt config.networking.interfaces."ens18".ipv6.addresses 0).address
-    "2a10:4741:36:29:6b:60ff:fe2a:7156" # slaac address, hardcoded for now
+    (builtins.elemAt config.networking.interfaces."enp10s0.29".ipv4.addresses 0).address
+    (builtins.elemAt config.networking.interfaces."enp10s0.29".ipv6.addresses 0).address
+    "2a10:4741:36:29:202:c9ff:fe9d:c36a" # slaac address, hardcoded for now
   ];
   services.tdude.kubernetes.worker.clusterCidrIpv4 = "10.234.128.0/18";
   services.tdude.kubernetes.worker.clusterCidrIpv6 = "2a10:4741:36:32:2::/104";
