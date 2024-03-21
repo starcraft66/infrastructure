d2029bdb37c7faa65be962db54a718ba8f35be82
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-03-21 02:31:47 -0400

    lava: actually enable ndppd

--- nixos/hosts/lava/containers.nix
+++ nixos/hosts/lava/containers.nix
@@ -10,24 +10,34 @@
   };
 
   # interfaces created by podman/docker compose bridges
-  services.ndppd.proxies."enp7s0".rules = {
-    "2a01:4f9:3051:104f:7::/80" = {
-      interface = "br-matrix";
-    };
-    "2a01:4f9:3051:104f:8::/80" = {
-      interface = "br-traefik";
-    };
-    "2a01:4f9:3051:104f:9::/80" = {
-      interface = "br-kerio";
-    };
-    "2a01:4f9:3051:104f:1d0::/80" = {
-      interface = "wg0";
-    };
-    "2a01:4f9:3051:104f:1d1::/80" = {
-      interface = "wg0";
-    };
-    "2a01:4f9:3051:104f:1df::/80" = {
-      interface = "wg0";
+  services.ndppd = {
+    enable = true;
+    interface = "enp7s0";
+    proxies."enp7s0".rules = {
+      "2a01:4f9:3051:104f:7::/80" = {
+        method = "iface";
+        interface = "br-matrix";
+      };
+      "2a01:4f9:3051:104f:8::/80" = {
+        method = "iface";
+        interface = "br-traefik";
+      };
+      "2a01:4f9:3051:104f:9::/80" = {
+        method = "iface";
+        interface = "br-kerio";
+      };
+      # "2a01:4f9:3051:104f:1d0::/80" = {
+      #   method = "iface";
+      #   interface = "wg0";
+      # };
+      # "2a01:4f9:3051:104f:1d1::/80" = {
+      #   method = "iface";
+      #   interface = "wg0";
+      # };
+      # "2a01:4f9:3051:104f:1df::/80" = {
+      #   method = "iface";
+      #   interface = "wg0";
+      # };
     };
   };
 }
