942655712ab19b2af929abecaddd26c7dddef6db
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-09-25 01:17:22 -0400

    stone: fix nix flake check

--- nixos/hosts/stone/configuration.nix
+++ nixos/hosts/stone/configuration.nix
@@ -27,5 +27,13 @@
   services.openssh.enable = true;
   services.prometheus.exporters.node.openFirewall = lib.mkForce false;
 
+  # Clash between redis and nixos-generators vm image
+  #  error: The option `boot.kernel.sysctl."vm.overcommit_memory"' is defined multiple times while it's expected to be unique.
+  #  Definition values:
+  #  - In `/nix/store/1h99qq6970gkx3j0m9w4yrrl9y99y1nk-source/nixos/modules/services/databases/redis.nix': "1"
+  #  - In `/nix/store/1h99qq6970gkx3j0m9w4yrrl9y99y1nk-source/nixos/modules/profiles/installation-device.nix': "1"
+  #  Use `lib.mkForce value` or `lib.mkDefault value` to change the priority on any of these definitions.
+  boot.kernel.sysctl."vm.overcommit_memory" = lib.mkForce "1";
+
   system.stateVersion = "23.11";
 }
