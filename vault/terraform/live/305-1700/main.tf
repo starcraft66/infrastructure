53d5327cc6ac456bc93bd077658f6c5da7d4beae
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-09-30 12:23:03 -0400

    argocd, vault: fix k8s-305-1700 redirect urls

--- vault/terraform/live/305-1700/main.tf
+++ vault/terraform/live/305-1700/main.tf
@@ -14,8 +14,8 @@ module "oidc_k8s" {
   source = "../../modules/oidc_k8s"
 
   cluster_id                         = "k8s-305-1700-1"
-  grafana_redirect_uri               = "https://monitoring.tdude.co/login/generic_oauth"
-  argocd_redirect_uri                = "https://gitops.tdude.co/auth/callback"
-  oauth2_proxy_redirect_uri          = "https://auth.k8s.235.tdude.co/oauth2/callback"
+  grafana_redirect_uri               = "https://monitoring.305-1700.tdude.co/login/generic_oauth"
+  argocd_redirect_uri                = "https://gitops.305-1700.tdude.co/auth/callback"
+  oauth2_proxy_redirect_uri          = "https://auth.k8s.305-1700.tdude.co/oauth2/callback"
   oidc_allowed_users_assignment_name = module.vault_oidc.oidc_allowed_users_assignment_name
 }
