9b075f9c18731810030ca80a234f5c963097a274
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-09-30 22:28:04 -0400

    spike: fix merge confict

--- nixos/hosts/spike/kubernetes.nix
+++ nixos/hosts/spike/kubernetes.nix
@@ -6,16 +6,9 @@
     ../../modules/k8s-305-1700-1
   ];
 
-<<<<<<< Updated upstream
   services.tdude.k8s-305-1700-1 = {
     enable = true;
     primaryNetworkInterface = "enp8s0";
     slaacAddress = "2a10:4741:37:51:e61d:2dff:fe27:dc10";
-=======
-  services.tdude.k8s-235-1 = {
-    enable = true;
-    primaryNetworkInterface = "eno3.29";
-    slaacAddress = "2a10:4741:36:29:26be:5ff:fe84:f750";
->>>>>>> Stashed changes
   };
 }
