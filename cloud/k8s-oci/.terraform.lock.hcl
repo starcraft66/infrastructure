312b89de8520af62914e0d73cb58de61e3b9a14d
Tristan Gosselin-Hane <starcraft66@gmail.com>
2026-02-25 22:29:00 -0500

    chore: switch VCN module source from local path to git reference
    Point to starcraft66/terraform-oci-vcn on GitHub instead of a relative
    local path, so the infrastructure is self-contained and reproducible.

--- cloud/k8s-oci/.terraform.lock.hcl
+++ cloud/k8s-oci/.terraform.lock.hcl
@@ -1,23 +1,11 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.opentofu.org/oracle/oci" {
-  version = "8.2.0"
+provider "registry.terraform.io/oracle/oci" {
+  version     = "8.2.0"
+  constraints = ">= 4.67.3, >= 6.32.0, 8.2.0"
   hashes = [
-    "h1:2G0brzVcMyDsn7HqoHCdpUyoJyPGj/wP+F00Crvi1Gg=",
-    "h1:G6yc4rImMpLVdbFIn9ZWm5Ts9GpFUzoybImgOZgfBGA=",
-    "h1:J+VihhA6znlPdjSCcIrSLMPjjkaEQWXZpETL1BVQCOQ=",
-    "h1:MkQ+ni3HlIsbb0rraDLNu0NExVcyoKAkxlc/GzaKx6A=",
-    "h1:RO1ogv5R/ZegY7wf5Xqs3lC8C9GnQZgjuQeVVhRSV0c=",
-    "h1:RjcSniuOLb2yduPdwEs7uayyVqhZzSiVhGcL8bKmZI4=",
-    "h1:cMNfM4cffPtOh38sfXzKJ1nC/Ph5G7Qihz1f7EdUFCQ=",
-    "h1:dIQ0OVFRF7OE5qicAxNQU0gotEf9aVm8+Vvi71uiO9w=",
-    "h1:gN8mxKz2QCjx1k6CzN9nyGmwvDYdUo57p93tOF1YLMs=",
-    "h1:htADROU5+VBzEvw9XFif25Y1UdISH9BmUNQOmFPzEUc=",
     "h1:nmYDzau7P6IzE49ocW+lc9X5d2quDFLh35Fz0RZZqR0=",
-    "h1:wEBSybgEodkdh1mjP5Syn/jNzguinUBG1O1tslZj7rQ=",
-    "h1:wqFbDPlidPhSg407hcnlumfqEOiI6Xe5CR6LplASYs0=",
-    "h1:ytPIgPgzfSgTl7ojZ9Cjtubs6kZYElZVPN9DFCESuGo=",
     "zh:16205629c327fa9271ca52bca2f171f699013d76ba4ee9809f1d70a90df55229",
     "zh:17279ab00a26ed08839ed29462d15ee1dddf558bccb659f1092bd599e370c4e7",
     "zh:20a11bc85497e9bfa4d42d906901a5fc692bd91b43d88ce8a16e930765a69cfe",
@@ -33,28 +21,5 @@ provider "registry.opentofu.org/oracle/oci" {
     "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
     "zh:b9bc714acf05389261773003eae4e3f2d5899564704b22372f8baacd6021b903",
     "zh:d0c092f2d89876f4a917949aa41d615953d2f302cbf2d5f829f85b1aed815c1a",
-  ]
-}
-
-provider "registry.terraform.io/oracle/oci" {
-  version     = "7.19.0"
-  constraints = ">= 4.67.3, >= 6.32.0, 7.19.0"
-  hashes = [
-    "h1:DSahVysS0IDbiQL3LpmMrqIp0sG4YIEuUDMARVzzCeY=",
-    "zh:214f8efa0a7c926ff9480c6fdce33159ca786cc42fdd3be9e6d74408c40005e2",
-    "zh:2a549ecf0a39008ce7201591d182da918c0f308baf0d90f652a8c39470586a7d",
-    "zh:334d96efd9036f2593b97906c56c92264fca406821ac5982973464229b55b017",
-    "zh:3c04dd68f2bf5da9de5c2988df20a7985df083159b5e96ada4fdba424d07f357",
-    "zh:5591f1379ccefcfd90ca1ef31ce1f51d978f40c3d9c8ed7fd24dbcfb1525eb19",
-    "zh:629864dd036736093270313b38218db956e2a499807b54f924904500c0b36866",
-    "zh:64390a5aa76ed07c5adff3f95a1e6b8db21707f49c93be616e2d0969d9b7b331",
-    "zh:74e6209b9f7069e7a0fc27e77841fa602df2901e84dfad25cff974e5313df0e2",
-    "zh:9111f1a3addd5e556f59c4219a076ce3a7c1f9a355557d7aac67935471b934f7",
-    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
-    "zh:bc728c3079c6f6e5c5793ae37e3800f215e4279c437a2e979f0418012be1478e",
-    "zh:c3226e2d8cc6850a93f495eca7f022ed6bc16bf5223acd4f3c94b47000c16bc3",
-    "zh:cb6976b6e4270e3259565f6e2c3c4cfe7421a2b546bd2c7df236318b35026253",
-    "zh:dab2900c0c5411845513f3673a35336a695ce479de94ec85ee7953071c2df91b",
-    "zh:fde92c507646609024afcc9dde5fe02ee4aab01dd93632273a112d17ef66bb75",
   ]
 }
--- cloud/k8s-oci/network.tf
+++ cloud/k8s-oci/network.tf
@@ -1,7 +1,7 @@
 # Virtual Network
 
 module "vcn" {
-  source = "../../../terraform-oci-vcn"
+  source = "git::https://github.com/starcraft66/terraform-oci-vcn.git?ref=main"
 
   compartment_id = var.compartment_id
   tenancy_id     = var.tenancy_id
@@ -26,16 +26,14 @@ module "vcn" {
 }
 
 # Subnets
+# Dual-stack OKE requires ALL subnets to be public:
+# https://docs.oracle.com/en-us/iaas/Content/ContEng/Tasks/conteng_ipv4-and-ipv6.htm
 
 resource "oci_core_subnet" "vcn_worker_subnet" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   cidr_block     = "10.233.192.0/24"
-
-  # IGW route table: dual-stack OKE requires public subnets for workers
-  # IGW handles both IPv4 and IPv6 default routes
   route_table_id = module.vcn.ig_route_id
-
   security_list_ids = [
     oci_core_security_list.worker_subnet_sl.id,
     oci_core_security_list.nlb_worker_subnet_sl.id
@@ -58,14 +56,30 @@ resource "oci_core_subnet" "vcn_public_subnet" {
   ipv6cidr_blocks = [cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 1)]
 }
 
+resource "oci_core_subnet" "vcn_pod_subnet" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  cidr_block     = "10.233.194.0/24"
+  route_table_id = module.vcn.ig_route_id
+  security_list_ids = [
+    oci_core_security_list.pod_subnet_sl.id
+  ]
+  display_name = "k8s-pod-subnet"
+
+  # IPv6: third /64 from the VCN's Oracle GUA /56
+  ipv6cidr_blocks = [cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 2)]
+}
+
 # Security Lists
 
+# Worker node subnet security list
+# https://docs.oracle.com/en-us/iaas/Content/ContEng/Concepts/contengnetworkconfig.htm
 resource "oci_core_security_list" "worker_subnet_sl" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   display_name   = "k8s-worker-subnet-sl"
 
-  # IPv4 egress everywhere
+  # Egress: allow all IPv4
   egress_security_rules {
     stateless        = false
     destination      = "0.0.0.0/0"
@@ -73,7 +87,7 @@ resource "oci_core_security_list" "worker_subnet_sl" {
     protocol         = "all"
   }
 
-  # IPv6 egress everywhere
+  # Egress: allow all IPv6
   egress_security_rules {
     stateless        = false
     destination      = "::/0"
@@ -81,29 +95,74 @@ resource "oci_core_security_list" "worker_subnet_sl" {
     protocol         = "all"
   }
 
-  # IPv4 ingress from VCN CIDR
+  # Ingress: worker-to-worker (IPv4)
   ingress_security_rules {
     stateless   = false
-    source      = "10.233.192.0/18"
+    source      = "10.233.192.0/24"
     source_type = "CIDR_BLOCK"
     protocol    = "all"
   }
 
-  # IPv6 ingress from VCN IPv6 CIDR
+  # Ingress: worker-to-worker (IPv6)
   ingress_security_rules {
     stateless   = false
-    source      = module.vcn.vcn_ipv6cidr_blocks[0]
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 0)
     source_type = "CIDR_BLOCK"
     protocol    = "all"
+  }
+
+  # Ingress: pods to workers (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.194.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: pods to workers (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 2)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: K8s API endpoint to workers (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.193.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: K8s API endpoint to workers (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 1)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: ICMP path discovery
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = "1"
+    icmp_options {
+      type = 3
+      code = 4
+    }
   }
 }
 
+# K8s API endpoint / LB public subnet security list
 resource "oci_core_security_list" "public_subnet_sl" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   display_name   = "k8s-public-subnet-sl"
 
-  # IPv4 egress everywhere
+  # Egress: allow all IPv4
   egress_security_rules {
     stateless        = false
     destination      = "0.0.0.0/0"
@@ -111,7 +170,7 @@ resource "oci_core_security_list" "public_subnet_sl" {
     protocol         = "all"
   }
 
-  # IPv6 egress everywhere
+  # Egress: allow all IPv6
   egress_security_rules {
     stateless        = false
     destination      = "::/0"
@@ -119,23 +178,39 @@ resource "oci_core_security_list" "public_subnet_sl" {
     protocol         = "all"
   }
 
-  # IPv4 ingress from VCN CIDR
+  # Ingress: from worker nodes (IPv4)
   ingress_security_rules {
     stateless   = false
-    source      = "10.233.192.0/18"
+    source      = "10.233.192.0/24"
     source_type = "CIDR_BLOCK"
     protocol    = "all"
   }
 
-  # IPv6 ingress from VCN IPv6 CIDR
+  # Ingress: from worker nodes (IPv6)
   ingress_security_rules {
     stateless   = false
-    source      = module.vcn.vcn_ipv6cidr_blocks[0]
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 0)
     source_type = "CIDR_BLOCK"
     protocol    = "all"
   }
 
-  # IPv4 ingress K8s API from internet
+  # Ingress: from pods (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.194.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: from pods (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 2)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: K8s API from internet (IPv4)
   ingress_security_rules {
     stateless   = false
     source      = "0.0.0.0/0"
@@ -147,7 +222,7 @@ resource "oci_core_security_list" "public_subnet_sl" {
     }
   }
 
-  # IPv6 ingress K8s API from internet
+  # Ingress: K8s API from internet (IPv6)
   ingress_security_rules {
     stateless   = false
     source      = "::/0"
@@ -157,6 +232,78 @@ resource "oci_core_security_list" "public_subnet_sl" {
       min = 6443
       max = 6443
     }
+  }
+}
+
+# Pod subnet security list (VCN-Native pod networking)
+# https://docs.oracle.com/en-us/iaas/Content/ContEng/Concepts/contengnetworkconfig.htm#securitylistconfig__section_rules_for_pod_subnets
+resource "oci_core_security_list" "pod_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  display_name   = "k8s-pod-subnet-sl"
+
+  # Egress: allow all IPv4
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  # Egress: allow all IPv6
+  egress_security_rules {
+    stateless        = false
+    destination      = "::/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  # Ingress: K8s API endpoint to pods (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.193.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: K8s API endpoint to pods (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 1)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: worker nodes to pods (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.192.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: worker nodes to pods (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 0)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: pod-to-pod (IPv4)
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.233.194.0/24"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  # Ingress: pod-to-pod (IPv6)
+  ingress_security_rules {
+    stateless   = false
+    source      = cidrsubnet(module.vcn.vcn_ipv6cidr_blocks[0], 8, 2)
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
   }
 }
 
