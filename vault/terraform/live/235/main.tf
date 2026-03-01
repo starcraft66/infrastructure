75ca7acd5b12dfc390517714295dd519289dcdb1
Tristan Gosselin-Hane <starcraft66@gmail.com>
2026-02-28 21:45:19 -0500

    argocd: switch to gitops.235.tdude.co for 235 cluster

--- vault/terraform/live/235/main.tf
+++ vault/terraform/live/235/main.tf
@@ -16,7 +16,7 @@ module "oidc_k8s" {
 
   cluster_id                         = "k8s-235-1"
   grafana_redirect_uri               = "https://monitoring.235.tdude.co/login/generic_oauth"
-  argocd_redirect_uri                = "https://gitops.tdude.co/auth/callback"
+  argocd_redirect_uri                = "https://gitops.235.tdude.co/auth/callback"
   oauth2_proxy_redirect_uri          = "https://auth.k8s.235.tdude.co/oauth2/callback"
   envoy_gateway_redirect_uri         = "https://dummy.235.tdude.co/oauth2/callback"
   oidc_allowed_users_assignment_name = module.vault_oidc.oidc_allowed_users_assignment_name
@@ -27,7 +27,7 @@ module "pocketid_oidc_k8s" {
 
   cluster_id                         = "k8s-235-1"
   grafana_redirect_uri               = "https://monitoring.235.tdude.co/login/generic_oauth"
-  argocd_redirect_uri                = "https://gitops.tdude.co/auth/callback"
+  argocd_redirect_uri                = "https://gitops.235.tdude.co/auth/callback"
   oauth2_proxy_redirect_uri          = "https://auth.k8s.235.tdude.co/oauth2/callback"
   envoy_gateway_redirect_uri         = "https://dummy.235.tdude.co/oauth2/callback"
 }
